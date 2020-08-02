d04b74e2892a05659e5fbeb3b477427677e1833c
Flamefusion <meshekharbehera613@gmail.com>
2020-08-02 22:04:26 +0530

    sdm660-common: build prebuilt kimcy screen recorder kimcy recorder version: 1.2.3.7 Signed-off-by: Flamefusion <meshekharbehera613@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -414,6 +414,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# Screen Recorder
+PRODUCT_PACKAGES += \
+    ScreenRecorder
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
