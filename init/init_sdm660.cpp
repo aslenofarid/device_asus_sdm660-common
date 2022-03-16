7c24adc5375ad9a48a16880042b72bd5f06c8e38
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2022-03-17 05:38:12 +0700

    sdm660-common: init: Update SafetyNet Workaround

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -109,8 +109,16 @@ void vendor_load_properties()
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 
    // Safetynet Workaround
+    property_override("ro.debuggable", "0");
+    property_override("ro.build.type", "user");
+    property_override("ro.build.tags", "release-keys");
+    property_override("ro.build.selinux", "1");
+    property_override("ro.boot.warranty_bit", "0");
+    property_override("ro.warranty_bit", "0");
+    property_override("ro.secure", "1");
     property_override("ro.boot.flash.locked", "1");
     property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.boot.selinux", "enforcing");
     property_override("ro.boot.veritymode", "enforcing");
     property_override("ro.boot.vbmeta.device_state", "locked");
     property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
