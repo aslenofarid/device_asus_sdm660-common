7a0c45c9d4c9325f7f040fb7ce04f913d28dfa81
Kunmun <kunmun@aospa.co>
2023-02-23 07:28:47 +0530

    sdm660-common: BoardConfig: Move platform flag to makefile
    Change-Id: Ib0760588816f1c1f346fe0bd0a092a5b86a5a379

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,9 +102,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /mnt/vendor/persist:/persist
 
-# Platform
-TARGET_BOARD_PLATFORM := sdm660
-
 # Power
 TARGET_USES_INTERACTION_BOOST := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
@@ -115,9 +112,6 @@ TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
-
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
