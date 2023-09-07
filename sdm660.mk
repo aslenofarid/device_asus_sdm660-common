Whot1966/android_device_asus_sdm660-common

sdm660-common: Add touchscreen gestures HIDL HAL
Change-Id: I02380e387f01fe20f984d2e6daa095f2056c03f9

sdm660.mk
@@ -465,6 +465,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.X00TD
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic

touch/service.cpp
@@ -1,0 +1,35 @@
+//
+// Copyright (C) 2022 The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.X00TD"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "TouchscreenGesture.h"
+
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}

aslenofarid/device_asus_sdm660-common

sdm660.mk
@@ -429,6 +429,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.asus_sdm660
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic

touch/service.cpp
@@ -1,0 +1,35 @@
+//
+// Copyright (C) 2022 The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.asus_sdm660"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "TouchscreenGesture.h"
+
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}