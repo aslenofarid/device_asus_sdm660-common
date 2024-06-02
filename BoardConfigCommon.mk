7803ec2766d120bf8ca8a25505475738684a431d
Dyneteve <sound0020@gmail.com>
2024-06-03 01:32:41 +0530

    sdm660-common: Enable AOSP surfaceflinger * AOSP SurfaceFlinger is smoother than CAF

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,6 +167,9 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Treble
 BOARD_VNDK_VERSION := current
 
